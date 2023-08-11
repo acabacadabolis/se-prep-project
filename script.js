c0768fa98d566c7649ee8b4a88d289412a3c2b19
Kerem Deen <Kerem.deen@gmail.com>
2023-08-11 00:40:21 -0400

    add js file, connect to html

---
+++ script.js
@@ -0,0 +1,9 @@
+//const h2 = document.createElement("h2");
+
+//h2.textContent = "try again";
+
+//document.querySelector("body").appendChild(h2);
+
+const h4 = document.createElement("h4");
+h4.textContent = "Nice try :)";
+document.querySelector("body").appendChild(h4);
